6ea054622f02ff987343f1313fb03f97716ffc68
nickamorim <nickamorim98@gmail.com>
2018-01-13 15:09:46 -0500

    initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/api/:msg', function (req, res) {
+  console.log('got message: ' + req.params.msg)
+  res.sendStatus(200)
+})
+
+app.listen(5000, function () {
+  console.log('Example app listening on port 5000!')
+})
